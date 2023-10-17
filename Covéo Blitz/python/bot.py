7c537ab84d0f3a7d431a71ae026ed0afb65358cb
kcnq <2135433@cegeplimoilou.ca>
2023-10-17 16:31:36 -0400

    Bot 3600

--- Covéo Blitz/python/bot.py
+++ Covéo Blitz/python/bot.py
@@ -1,3 +1,4 @@
+import game_message
 from game_message import *
 from actions import *
 from random import choice
@@ -7,16 +8,39 @@ from scipy.optimize import fsolve
 
 
 class Bot:
+    shot_meteors = []
+    game_message = None
+
     def __init__(self):
         self.direction = 1
         print("Initializing your super mega duper bot")
+
+    def choose_meteor(self):
+        target = self.game_message.meteors[0]
+        for meteor in self.game_message.meteors:
+            valid = True
+            for id in self.shot_meteors:
+                if meteor.id == id or meteor.position.x < (self.game_message.cannon.position.x + 150):
+                    valid = False
+            if valid:
+                if meteor.size == game_message.MeteorType.Small:
+                    return target
+                elif target is None:
+                    target = meteor
+                else:
+                    target = meteor
+                # elif target.position.x > meteor.position.x:
+                #     target = meteor
+        return target
 
     def get_next_move(self, game_message: GameMessage):
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
-        meteor = choice(game_message.meteors)
-        #Système d'équation non linéaire à résoudre
+        self.game_message = game_message
+        meteor = self.choose_meteor()
+
+        # Système d'équation non linéaire à résoudre
         def func(x):
             return [((game_message.constants.rockets.speed * np.cos(x[0]) - meteor.velocity.x) *
                      x[1] + game_message.cannon.position.x - meteor.position.x),
@@ -26,16 +50,14 @@ class Bot:
 
         root = fsolve(func, [1, 10])
         if game_message.cannon.cooldown == 0:
+            self.shot_meteors.append(meteor.id)
             return [
-                    LookAtAction(target=Vector(meteor.velocity.x*root[1]+meteor.position.x, meteor.velocity.y*root[1]+meteor.position.y)),
-                    ShootAction(),
-                ]
+                LookAtAction(target=Vector(meteor.velocity.x * root[1] + meteor.position.x,
+                                           meteor.velocity.y * root[1] + meteor.position.y)),
+                ShootAction(),
+            ]
         else:
             return [
-                LookAtAction(target=Vector(meteor.velocity.x*root[1]+meteor.position.x, meteor.velocity.y*root[1]+meteor.position.y)),
+                LookAtAction(target=Vector(meteor.velocity.x * root[1] + meteor.position.x,
+                                           meteor.velocity.y * root[1] + meteor.position.y)),
             ]
-
-
-
-
-
