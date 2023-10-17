900de8cb4a716238c714e228ed924e2cb79e544d
Kamran Charles Nayebi <2135433@cegeplimoilou.ca>
2023-10-17 11:49:23 -0400

    Implémenté code pour tirer

--- Covéo Blitz/python/bot.py
+++ Covéo Blitz/python/bot.py
@@ -2,7 +2,8 @@ from game_message import *
 from actions import *
 from random import choice
 import math
-import numpy
+import numpy as np
+from scipy.optimize import fsolve
 
 
 class Bot:
@@ -15,17 +16,27 @@ class Bot:
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
 
-        cannon = game_message.cannon
-        meteor = choice(game_message.meteors)
+        # cannon = game_message.cannon
+        # meteor = choice(game_message.meteors)
+        #
+        # norme_projectile = game_message.constants.rockets.speed
+        # norme_meteor = math.sqrt(meteor.velocity.x ** 2 + meteor.velocity.y ** 2)
+        #
+        # orientation_meteor = math.arctan(meteor.velocity.y / meteor.velocity.x)
 
-        norme_projectile = game_message.constants.rockets.speed
-        norme_meteor = math.sqrt(meteor.velocity.x**2 + meteor.velocity.y**2)
+        #Système d'équation non linéaire à résoudre
+        def func(x):
+            return [((game_message.constants.rockets.speed * np.cos(x[0]) - choice(game_message.meteors).velocity.x) *
+                     x[1] + game_message.cannon.position.x - choice(game_message.meteors).position.x),
 
-        orientation_meteor = math.arctan(meteor.velocity.y / meteor.velocity.x)
+                    ((game_message.constants.rockets.speed * np.sin(x[0]) - choice(game_message.meteors).velocity.y) *
+                     x[1] + game_message.cannon.position.y - choice(game_message.meteors).position.y)]
 
-
+        root = fsolve(func, [-4, 4])
 
         return [
-            LookAtAction(target=Vector()),
+            RotateAction(angle=root[0]),
             ShootAction(),
         ]
+
+
